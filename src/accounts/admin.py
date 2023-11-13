c5fc4e4e2a8dcd77bc4494ea13ae49f67f4feb72
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-11-13 10:25:18 +0330

    feat: add admin panel customization for users

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from accounts.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from accounts.models import User
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'fullname', 'is_admin')
+    list_filter = ('is_admin', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('fullname',)}),
+        ('Permissions', {'fields': ('is_admin', 'is_active')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email', 'fullname')
+    ordering = ('fullname',)
+    list_display_links = ('email', 'fullname', 'is_admin')
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
