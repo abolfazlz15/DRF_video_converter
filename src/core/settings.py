96a8a5c28d3a33402f259034f993f8d085fd44e5
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-12-01 18:14:11 +0330

    feat: add smtp config for send email

--- src/core/settings.py
+++ src/core/settings.py
@@ -91,7 +91,7 @@ if config('ENGINE') == 'sqlite3':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 elif config('ENGINE') == 'mysql':
@@ -173,7 +173,7 @@ REST_FRAMEWORK = {
     )
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
